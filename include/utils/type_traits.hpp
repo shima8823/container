7a44f4811c73e91e86b1dd636e77f97573f4e0c1
shima8823 <taiyaki1275@gmail.com>
2023-01-08 16:06:17 +0900

    [add] comment

--- include/utils/type_traits.hpp
+++ include/utils/type_traits.hpp
@@ -5,7 +5,8 @@ namespace ft {
 
 // integral_constant
 
-template <typename _Tp, _Tp __v>
+// _Tp型の__v
+template <class _Tp, _Tp __v>
 struct integral_constant {
 	static const _Tp value = __v;
 	typedef _Tp value_type;
@@ -16,59 +17,56 @@ struct integral_constant {
 typedef integral_constant<bool, true> true_type;
 typedef integral_constant<bool, false> false_type;
 
-// is_same
-
-template <typename, typename>
-struct is_same: public false_type {};
-template <typename _Tp>
-struct is_same<_Tp, _Tp> : public true_type {};
-
 // remove_const
 
-template <typename T>
+template <class T>
 struct remove_const {typedef T type;};
-template <typename T>
+template <class T>
 struct remove_const<const T> {typedef T type;};
 
 // remove_volatile
+// volatile compile時の最適化
 
-template <typename T>
+template <class T>
 struct remove_volatile {typedef T type;};
-template <typename T>
+template <class T>
 struct remove_volatile<volatile T> {typedef T type;};
 
 // remove_cv
 
-template <typename T>
+template <class T>
 struct remove_cv {
-	typedef typename remove_const<typename remove_volatile<T>::type>::type type;
+	typedef typename remove_volatile<typename remove_const<T>::type>::type type;
 };
 
 // helper
 
 template <class _Tp>
-struct is_integral_helper : public false_type {};
+struct _is_integral : public false_type {};
 
-template <> struct is_integral_helper<bool>					: public true_type {};
-template <> struct is_integral_helper<char>					: public true_type {};
-template <> struct is_integral_helper<signed char>			: public true_type {};
-template <> struct is_integral_helper<unsigned char>		: public true_type {};
-template <> struct is_integral_helper<wchar_t>				: public true_type {};
-template <> struct is_integral_helper<short>				: public true_type {};
-template <> struct is_integral_helper<unsigned short>		: public true_type {};
-template <> struct is_integral_helper<int>					: public true_type {};
-template <> struct is_integral_helper<unsigned int>			: public true_type {};
-template <> struct is_integral_helper<long>					: public true_type {};
-template <> struct is_integral_helper<unsigned long>		: public true_type {};
+// template <> 完全特殊化 それぞれの型に対して。
+template <> struct _is_integral<bool>				: public true_type {};
+template <> struct _is_integral<char>				: public true_type {};
+template <> struct _is_integral<signed char>		: public true_type {};
+template <> struct _is_integral<unsigned char>		: public true_type {};
+template <> struct _is_integral<wchar_t>			: public true_type {};
+template <> struct _is_integral<short>				: public true_type {};
+template <> struct _is_integral<unsigned short>		: public true_type {};
+template <> struct _is_integral<int>				: public true_type {};
+template <> struct _is_integral<unsigned int>		: public true_type {};
+template <> struct _is_integral<long>				: public true_type {};
+template <> struct _is_integral<unsigned long>		: public true_type {};
 
 // is_integral
 
 template <class _Tp>
-struct is_integral : public is_integral_helper<typename remove_cv<_Tp>::type>::type {};
+struct is_integral : public _is_integral<typename remove_cv<_Tp>::type> {};
 
 // enable_if
 
-template <bool, class _Tp = void> struct enable_if {};
+// false の場合
+template <bool B, class _Tp = void> struct enable_if {};
+// true の場合
 template <class _Tp> struct enable_if<true, _Tp> {typedef _Tp type;};
 
 } // namespace ft
